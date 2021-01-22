5188f8950eb33866187f4b894616f5549cdac300
anmahei <anne-mari.heiniola@myy.haaga-helia.fi>
2021-01-22 19:03:00 +0200

    first commit

---
+++ src/test/java/fi/tehtavakaksi/Viikkotehtava2/Viikkotehtava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tehtavakaksi.Viikkotehtava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Viikkotehtava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
